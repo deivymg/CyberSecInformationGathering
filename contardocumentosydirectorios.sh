df0225f3427261b26bccceda361be448cb591960
deivymg <deivymg@gmail.com>
2023-08-22 13:43:11 -0500

    Update contardocumentosydirectorios.sh

--- contardocumentosydirectorios.sh
+++ contardocumentosydirectorios.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 #script shell that calculate repetigions of foler in a file
+output_file="combined.csv"
 for file in *.txt; do
     if [ -f "$file" ]; then
         #step 1: extract paths from gathering file
         directorios=$(cat "$file"  |sed -n '/============ Archivos encontrados ============/,$p' | awk -F '  ' '{print  $3 }'  | sort -u  | sed 's![^/]*$!!' | sed 's/^[^/]*\///' | sed '/^\s*$/d' | sed 's/^/\//' | sort);
+        total=$(cat "$file" |sed -n '/============ Archivos encontrados ============/,$p' | wc -l);
+        total=$((total-1));
         #step 2: get directotris with 5 levels
         rootdir='';
-        for ((j=2; j<=5; j++)) do
-            rootdir+=$(echo "$directorios"| grep -E '([^/]*\/){'$j',}' | awk -F/ 'BEGIN{OFS="/"} {for(i=1; i<='$j'; i++) printf "%s%s", $i, (i=='$j' ? "\n" : "/")}' | sort -u  | sed 's/$/\//' );
+        for ((j=5; j<=5; j++)) do
+            rootdir+=$(echo "$directorios" | sort -u | grep -E '^(.*\/){'$j',}'| sed 's#^\(\([^/]*\/\)\{'$j'\}\).*#\1#' | sort -u) ;
+
         done
-        #step 3: count ocurrences of each directory
+        rootdir="$(echo "$rootdir" | sed 's#//#\n/#g' | sort -u)";
+        #step 3: count ocurrences of each directory and create file ouput
+        echo "=== $file ===" >> "$output_file";
         for dir in $rootdir; do
             count=$(echo "$directorios"| grep "$dir"| wc -l);
-            echo  "$count"";""$dir">>"$file"".csv"; 
-            #echo  'echo $directorios | grep -ic "'$dir'"';
+            echo  "$count"";""$dir">>"$output_file"; 
         done
-
-         #cat "$file" >> "$outputfile"
+        echo "TOTAL OF FOUNDED FILES:""$total" >> "$output_file";
     fi
 done
